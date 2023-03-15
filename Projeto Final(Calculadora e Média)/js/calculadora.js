08241c95886f81e27780673e04c8df6b5562c040
Dianne Carolayne <diannesilva01@gmail.com>
2023-03-15 12:49:08 -0300

    Projeto Final

---
+++ Projeto Final(Calculadora e Média)/js/calculadora.js
@@ -0,0 +1,64 @@
+function somar() {
+  const numero1 = parseFloat(document.getElementById("numero1Soma").value);
+  const numero2 = parseFloat(document.getElementById("numero2Soma").value);
+  // evite mexer no código acima!
+
+  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
+  Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
+  
+    let soma = numero1 + numero2;
+    
+  
+  
+  
+  
+  
+  document.getElementById("resultadoSoma").innerHTML = soma;
+  
+}
+
+function subtrair() {
+  const numero1 = parseFloat(document.getElementById("numero1Sub").value);
+  const numero2 = parseFloat(document.getElementById("numero2Sub").value);
+  // evite mexer no código acima!
+
+  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
+   Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
+  
+   let subtracao = numero1 - numero2;
+  
+  
+  
+  
+
+  document.getElementById("resultadoSub").innerHTML = subtracao;
+}
+
+function multiplicar() {
+  const numero1 = parseFloat(document.getElementById("numero1Mult").value);
+  const numero2 = parseFloat(document.getElementById("numero2Mult").value);
+  // evite mexer no código acima!
+
+  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
+   Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
+  
+  let multiplicacao= numero1 * numero2
+  
+  
+  document.getElementById("resultadoMult").innerHTML = multiplicacao;
+}
+
+function dividir() {
+  const numero1 = parseFloat(document.getElementById("numero1Div").value);
+  const numero2 = parseFloat(document.getElementById("numero2Div").value);
+  // evite mexer no código acima!
+
+  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
+   Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
+  
+  let divisao = numero1 / numero2
+  
+  
+  
+  document.getElementById("resultadoDiv").innerHTML = divisao;
+}
